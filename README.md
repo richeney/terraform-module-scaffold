d369fea5bbb29bd166ae74bf90585bf23d1e694c
Richard Cheney <richard.cheney@microsoft.com>
2018-09-04 11:49:45 +0100

    New scaffold module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # terraform-modules
+
+Modules used by lab7 of https://aka.ms/citadel/terraform
---
+++ scaffold/main.tf
@@ -0,0 +1,186 @@
+resource "azurerm_resource_group" "core" {
+    name        = "core"
+    location    = "${var.loc}"
+    tags        = "${var.tags}"
+}
+
+
+resource "azurerm_public_ip" "vpnGatewayPublicIp" {
+    name                = "vpnGatewayPublicIp"
+    location            = "${azurerm_resource_group.core.location}"
+    resource_group_name = "${azurerm_resource_group.core.name}"
+    tags                = "${azurerm_resource_group.core.tags}"
+
+    public_ip_address_allocation = "dynamic"
+}
+
+resource "azurerm_virtual_network" "core" {
+   name                 = "core"
+   location             = "${azurerm_resource_group.core.location}"
+   resource_group_name  = "${azurerm_resource_group.core.name}"
+   tags                 = "${azurerm_resource_group.core.tags}"
+
+   address_space        = [ "10.0.0.0/16" ]
+   dns_servers          = [ "1.1.1.1", "1.0.0.1" ]
+}
+
+resource "azurerm_subnet" "GatewaySubnet" {
+   name                 = "GatewaySubnet"
+   resource_group_name  = "${azurerm_resource_group.core.name}"
+   virtual_network_name = "${azurerm_virtual_network.core.name}"
+
+   address_prefix       = "10.0.0.0/24"
+}
+
+resource "azurerm_subnet" "training" {
+   name                 = "training"
+   resource_group_name  = "${azurerm_resource_group.core.name}"
+   virtual_network_name = "${azurerm_virtual_network.core.name}"
+
+   address_prefix       = "10.0.1.0/24"
+}
+
+resource "azurerm_subnet" "dev" {
+   name                 = "dev"
+   resource_group_name  = "${azurerm_resource_group.core.name}"
+   virtual_network_name = "${azurerm_virtual_network.core.name}"
+
+   address_prefix       = "10.0.2.0/24"
+}
+
+/*
+Commented out to save time
+
+resource "azurerm_virtual_network_gateway" "vpnGateway" {
+    name                = "vpnGateway"
+    location            = "${azurerm_resource_group.core.location}"
+    resource_group_name = "${azurerm_resource_group.core.name}"
+    tags                = "${azurerm_resource_group.core.tags}"
+
+    type                = "Vpn"
+    vpn_type            = "RouteBased"
+
+    sku                 = "Basic"
+    enable_bgp          = true
+
+    ip_configuration {
+        name                            = "vpnGwConfig"
+        public_ip_address_id            = "${azurerm_public_ip.vpnGatewayPublicIp.id}"
+        private_ip_address_allocation   = "Dynamic"
+        subnet_id                       = "${azurerm_subnet.GatewaySubnet.id}"
+    }
+
+}
+*/resource "azurerm_resource_group" "nsgs" {
+   name         = "NSGs"
+   location     = "${var.loc}"
+   tags         = "${var.tags}"
+}
+
+resource "azurerm_network_security_group" "AllowSSH" {
+   name = "AllowSSH"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+}
+
+resource "azurerm_network_security_rule" "AllowSSH" {
+    name = "AllowSSH"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.AllowSSH.name}"
+
+    priority                    = 1001
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 22
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
+
+resource "azurerm_network_security_group" "AllowHTTP" {
+   name = "AllowHTTP"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+}
+
+resource "azurerm_network_security_rule" "AllowHTTP" {
+    name = "AllowHTTP"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.AllowHTTP.name}"
+
+    priority                    = 1001
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 80
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
+
+resource "azurerm_network_security_group" "AllowHTTPS" {
+   name = "AllowHTTPS"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+}
+
+resource "azurerm_network_security_rule" "AllowHTTPS" {
+    name = "AllowHTTPS"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.AllowHTTPS.name}"
+
+    priority                    = 1001
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 443
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
+resource "azurerm_network_security_group" "AllowSQLServer" {
+   name = "AllowSQLServer"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+}
+
+resource "azurerm_network_security_rule" "AllowSQLServer" {
+    name = "AllowSQLServer"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.AllowSQLServer.name}"
+
+    priority                    = 1001
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 1443
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
+resource "azurerm_network_security_group" "AllowRDP" {
+   name = "AllowRDP"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+}
+
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowRDP"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.AllowRDP.name}"
+
+    priority                    = 1001
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 3389
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
